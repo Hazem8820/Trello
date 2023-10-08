e2d93405bc0fe3e27a023e93ef4bf54a121c49b7
Hazem Galal <hazemgalal701@gmail.com>
2023-10-08 04:31:40 +0300

    final commit

--- src/modules/order/order.services.js
+++ src/modules/order/order.services.js
@@ -1,6 +1,6 @@
 import cartModel from "../../../DB/models/cart.model.js";
 import productModel from "../../../DB/models/product.model.js";
-import { asyncHandler } from "../../utils/globalerrorhandling.js";
+import { asyncHandler } from "../../utils/globalErrorHandling.js";
 
 export const clearCart = asyncHandler(async (userId) => {
     await cartModel.findOneAndUpdate({ user: userId }, { products: [] })
